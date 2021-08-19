8cb35db0271c485c13b0e3134db640c11a16b78b
Faruk Cesur <faruk-cesur@hotmail.com>
2021-08-19 16:59:17 +0300

    Added MK Toon & FinishLine and Script & Added Font

---
+++ Assets/Game/Scripts/NormalScripts/Finish.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController player = other.GetComponentInParent<PlayerController>();
+        if (player)
+        {
+        }
+    }
+}
