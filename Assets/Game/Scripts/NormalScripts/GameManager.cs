8373445558d1b1e00c38bfb66215d47e75c84495
Faruk Cesur <faruk-cesur@hotmail.com>
2021-08-19 15:28:04 +0300

    Added Empty Folders To Work On & Added GameManager

---
+++ Assets/Game/Scripts/NormalScripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    Prepare,
+    MainGame,
+    GameOver,
+    FinishGame,
+}
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public static Camera Cam;
+
+    private GameState _currentGameState;
+
+    public GameState CurrentGameState
+    {
+        get { return _currentGameState; }
+        set
+        {
+            switch (value)
+            {
+                case GameState.Prepare:
+                    break;
+                case GameState.MainGame:
+                    break;
+                case GameState.GameOver:
+                    break;
+                case GameState.FinishGame:
+                    break;
+                default:
+                    break;
+            }
+
+            _currentGameState = value;
+        }
+    }
+
+    private void Awake()
+    {
+        Cam = Camera.main;
+
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+        CurrentGameState = GameState.Prepare;
+    }
+}
