99dab715a53c96adc7aea8007fb2ff7e981ca095
Faruk Cesur <faruk-cesur@hotmail.com>
2021-08-22 00:43:13 +0300

    Updated Project

--- Assets/Game/Scripts/NormalScripts/Finish.cs
+++ Assets/Game/Scripts/NormalScripts/Finish.cs
@@ -2,17 +2,44 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Finish : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    [SerializeField] private Slider slider;
+    [SerializeField] private Transform currentPlayerPos;
+    private float playerStartPosZ, finishLinePosZ, totalPathLength;
+
+    private void Start()
     {
-        PlayerController player = other.GetComponentInParent<PlayerController>();
-        if (player)
+        playerStartPosZ = currentPlayerPos.position.z;
+        finishLinePosZ = transform.position.z;
+        totalPathLength = finishLinePosZ - playerStartPosZ;
+    }
+
+    private void Update()
+    {
+        switch (GameManager.Instance.CurrentGameState)
         {
-            AnimationController.instance.ActivateVictoryAnim();
-            player.PlayerSpeedDown();
-            GameManager.Instance.Win();
+            case GameState.Prepare:
+                break;
+            case GameState.MainGame:
+                CalculatePlayerDistance();
+                break;
+            case GameState.GameOver:
+                break;
+            case GameState.FinishGame:
+                break;
+        }
+    }
+
+    private void CalculatePlayerDistance()
+    {
+        float completedPathRatio = currentPlayerPos.position.z / totalPathLength;
+        slider.value = completedPathRatio;
+        if (completedPathRatio >= 1f)
+        {
+            Debug.Log("win");
             GameManager.Instance.CurrentGameState = GameState.FinishGame;
         }
     }
