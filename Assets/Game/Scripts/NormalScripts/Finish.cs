e1b930c05a5743d0fa97f12a806f3eac3843871b
Faruk Cesur <faruk-cesur@hotmail.com>
2021-08-22 00:16:40 +0300

    Revert "Merge branch 'main' of https://github.com/faruk-cesur/PickyRunner3D"
    This reverts commit 056ff12d5126a2a52c2083dd1a49322d8a371f0b, reversing
    changes made to 10cf248ef57772e22c0e308097c6d77ea61df480.

--- Assets/Game/Scripts/NormalScripts/Finish.cs
+++ Assets/Game/Scripts/NormalScripts/Finish.cs
@@ -2,42 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Finish : MonoBehaviour
 {
-    [SerializeField] private Slider slider;
-    [SerializeField] private Transform currentPlayerPos;
-    private float playerStartPosZ, finishLinePosZ, totalPathLength;
-
-    private void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        playerStartPosZ = currentPlayerPos.position.z;
-        finishLinePosZ = transform.position.z;
-        totalPathLength = finishLinePosZ - playerStartPosZ;
-    }
-
-    private void Update()
-    {
-        switch (GameManager.Instance.CurrentGameState)
-        {
-            case GameState.Prepare:
-                break;
-            case GameState.MainGame:
-                CalculatePlayerDistance();
-                break;
-            case GameState.GameOver:
-                break;
-            case GameState.FinishGame:
-                break;
-        }
-    }
-
-    private void CalculatePlayerDistance()
-    {
-        float completedPathRatio = currentPlayerPos.position.z / totalPathLength;
-        slider.value = completedPathRatio;
-        if (completedPathRatio >= 1f)
+        PlayerController player = other.GetComponentInParent<PlayerController>();
+        if (player)
         {
             AnimationController.instance.ActivateVictoryAnim();
             player.PlayerSpeedDown();
